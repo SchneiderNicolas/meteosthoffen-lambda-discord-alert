932cf3b215cc0543a03f49eb25d9f620ff9a3ba6
Nicolas Schneider <nicolas.schneider@epitech.eu>
2023-05-21 15:25:49 -0400

    feat: discord alert notification

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
-export const handler = async(event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda alert!'),
+const axios = require('axios');
+
+exports.handler = async (event) => {
+    const { discordWebhookUrl, message, stationId } = event;
+
+    if (!discordWebhookUrl || !message || !stationId) {
+        throw new Error('Missing discordWebhookUrl, message or stationId in event');
+    }
+
+    const embed = {
+        title: 'Weather Station Alert',
+        color: 16711680,
+        fields: [
+            {
+                name: 'Station ID',
+                value: stationId,
+                inline: true,
+            },
+            {
+                name: 'Message',
+                value: message,
+                inline: true,
+            },
+        ],
+        timestamp: new Date().toISOString(),
     };
-    return response;
+
+    try {
+        const discordResponse = await axios.post(discordWebhookUrl, { embeds: [embed] });
+        console.log(`Discord response status: ${discordResponse.status}`);
+    } catch (error) {
+        console.error(`Failed to send message to Discord: ${error}`);
+        throw error;
+    }
 };
